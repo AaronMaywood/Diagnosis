e7fda863d05a65ed5c84e201c2bbef520fd60a78
AaronMaywood <95473446+AaronMaywood@users.noreply.github.com>
2024-04-15 12:53:25 +0900

    bugfix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', "@pinia/nuxt"],
   app: {
-    baseURL: '/diagnosis/',
+    baseURL: '/Diagnosis/',
+    // 一般的なWebサーバーへデプロイする場合
     // baseURL: '/javascript/diagnosis/',
     // buildAssetsDir: '/javascript/diagnosis/_nuxt/',
   },
