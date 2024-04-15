b8cc843ad668f12e900b1b07c0740ee6aca5cef3
AaronMaywood <95473446+AaronMaywood@users.noreply.github.com>
2024-04-15 12:50:52 +0900

    github pages 用設定
    相対パスで動作するように調整

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', "@pinia/nuxt"]
+  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', "@pinia/nuxt"],
+  app: {
+    baseURL: '/diagnosis/',
+    // baseURL: '/javascript/diagnosis/',
+    // buildAssetsDir: '/javascript/diagnosis/_nuxt/',
+  },
 })
